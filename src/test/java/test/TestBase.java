36c90c3c810dc7e6a094dc74aefec222cb1495c2
Natalya.Kadysheva <Native1331>
2022-06-16 18:12:35 +0300

    Add Test

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -2,20 +2,12 @@ package test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import config.CredentialsConfig;
 import drivers.BrowserstackMobileDriver;
 import helpers.Attach;
-import io.opentelemetry.sdk.logs.data.LogDataBuilder;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.Attach.sessionId;
@@ -29,36 +21,11 @@ public class TestBase {
         Configuration.browserSize = null;
 
     }
-
-
-
-
-
-      /**  SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.browser = BrowserstackMobileDriver.class.getName();
-        Configuration.browserSize = null;
-        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
-       String selenoidLogin = config.selenoidLogin();
-       String selenoidPassword = config.selenoidPassword();
-       String selenoidServer = System.getProperty("selenoid_server", "selenoid.autotests.cloud/wd/hub");
-              Configuration.baseUrl = "https://hh.ru";
-              Configuration.browserSize = "1920x1080";
-     Configuration.remote = "https://" + selenoidLogin + ":" + selenoidPassword + "@" +
-                selenoidServer;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-        Configuration.browser = BrowserstackMobileDriver.class.getName();
-        Configuration.browserSize = null;
-    }**/
-
     @BeforeEach
     public void startDriver() {
         addListener("AllureSelenide", new AllureSelenide());
         open();
     }
-
     @AfterEach
     public void afterEach() {
         String sessionId = sessionId();
@@ -66,12 +33,8 @@ public class TestBase {
         Attach.pageSource();
         Attach.video(sessionId);
         step("Close driver", Selenide::closeWebDriver);
-
         Attach.video(sessionId);
-        //     Attach.browserConsoleLogs();
-        //      Attach.addVideo();
-      //  closeWebDriver();
-    }
+           }
 
 }
 
