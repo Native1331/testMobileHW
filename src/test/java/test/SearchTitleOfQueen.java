3af34e9eceaefc782a5b027067151ff95633f846
Natalya.Kadysheva <Native1331>
2022-06-16 16:07:03 +0300

    Initial commit

---
+++ src/test/java/test/SearchTitleOfQueen.java
@@ -0,0 +1,30 @@
+package test;
+
+import com.codeborne.selenide.CollectionCondition;
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+
+public class SearchTitleOfQueen extends TestBase {
+
+
+    @Test
+   @Tag("android")
+    void searchTittleOfQueenTest() {
+    back();
+    step("Open search page", () -> {
+    $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+    $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Queen");
+    });
+    step("Verify content found", () ->
+    $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+    .shouldHave(CollectionCondition.sizeGreaterThan(0)));
+    }
+}
+
+
+
---
+++ src/test/java/test/TestBase.java
@@ -0,0 +1,72 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
+import drivers.BrowserstackMobileDriver;
+import helpers.Attach;
+import io.opentelemetry.sdk.logs.data.LogDataBuilder;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.Attach.sessionId;
+
+
+public class TestBase {
+
+
+
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+   //     Configuration.browser = BrowserstackMobileDriver.class.getName();
+   //     Configuration.browserSize = null;
+   //     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    //    String selenoidLogin = config.selenoidLogin();
+    //    String selenoidPassword = config.selenoidPassword();
+     //   String selenoidServer = System.getProperty("selenoid_server", "selenoid.autotests.cloud/wd/hub");
+        //      Configuration.baseUrl = "https://hh.ru";
+        //      Configuration.browserSize = "1920x1080";
+      //  Configuration.remote = "https://" + selenoidLogin + ":" + selenoidPassword + "@" +
+     //           selenoidServer;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = sessionId();
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.video(sessionId);
+        //     Attach.browserConsoleLogs();
+        //      Attach.addVideo();
+        closeWebDriver();
+    }
+
+}
+
+
+
+
+
